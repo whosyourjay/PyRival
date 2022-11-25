954b5533ebc772e09b1f2290a4008247c966eca6
whosyourjay <whosyourjay@users.noreply.github.com>
2022-11-25 11:35:32 -0500

    More naming in Dinic

--- pyrival/graphs/dinic.py
+++ pyrival/graphs/dinic.py
@@ -5,21 +5,21 @@ class Dinic:
     def __init__(self, n):
         self.lvl = [0] * n
         self.ptr = [0] * n
-        self.q = [0] * n
+        self.cue = [0] * n
         self.adj = [[] for _ in range(n)]
 
     def add_edge(self, a, b, cap, rcap=0):
         self.adj[a].append([b, len(self.adj[b]), cap, 0])
         self.adj[b].append([a, len(self.adj[a]) - 1, rcap, 0])
 
-    def dfs(self, vert, targ, limit):
-        if vert == targ or not limit:
+    def dfs(self, vert, dest, limit):
+        if vert == dest or not limit:
             return limit
 
         for pos in range(self.ptr[vert], len(self.adj[vert])):
             nei, pair, cap, use = self.adj[vert][pos]
             if self.lvl[nei] == self.lvl[vert] + 1:
-                aug = self.dfs(nei, targ, min(limit, cap - use))
+                aug = self.dfs(nei, dest, min(limit, cap - use))
                 if aug:
                     self.adj[vert][pos][3] += aug
                     self.adj[nei][pair][3] -= aug
@@ -28,27 +28,27 @@ class Dinic:
 
         return 0
 
-    def calc(self, start, targ):
-        flow, self.q[0] = 0, start
+    def calc(self, src, dest):
+        flow, self.cue[0] = 0, src
         for l in range(31):  # l = 30 maybe faster for random data
             while True:
-                self.lvl, self.ptr = [0] * len(self.q), [0] * len(self.q)
-                qi, qe, self.lvl[start] = 0, 1, 1
-                while qi < qe and not self.lvl[targ]:
-                    vert = self.q[qi]
+                self.lvl, self.ptr = [0] * len(self.cue), [0] * len(self.cue)
+                qi, qe, self.lvl[src] = 0, 1, 1
+                while qi < qe and not self.lvl[dest]:
+                    vert = self.cue[qi]
                     qi += 1
                     for nei, pair, cap, use in self.adj[vert]:
                         if not self.lvl[nei] and (cap - use) >> (30 - l):
-                            self.q[qe] = nei
+                            self.cue[qe] = nei
                             qe += 1
                             self.lvl[nei] = self.lvl[vert] + 1
 
-                aug = self.dfs(start, targ, INF)
+                aug = self.dfs(src, dest, INF)
                 while aug:
                     flow += aug
-                    aug = self.dfs(start, targ, INF)
+                    aug = self.dfs(src, dest, INF)
 
-                if not self.lvl[targ]:
+                if not self.lvl[dest]:
                     break
 
         return flow
