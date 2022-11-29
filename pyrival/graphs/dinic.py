908ce01fc2031b4500e65a66c3ed42595dbf186d
whosyourjay <whosyourjay@users.noreply.github.com>
2022-11-29 09:55:08 -0500

    Revert globals, tested on SPOJ

--- pyrival/graphs/dinic.py
+++ pyrival/graphs/dinic.py
@@ -1,20 +1,22 @@
 INF = float("inf")
 
-graph = [[] for _ in range(n)]
-lvl = [0] * n
-ptr = [0] * n
 class Dinic:
+    def __init__(self, n):
+        self.lvl = [0] * n
+        self.ptr = [0] * n
+        self.cue = [0] * n
+        self.adj = [[] for _ in range(n)]
+
     def add_edge(self, a, b, cap, rcap=0):
-        graph[a].append([b, len(graph[b]), cap, 0])
-        graph[b].append([a, len(graph[a]) - 1, rcap, 0])
+        self.adj[a].append([b, len(self.adj[b]), cap, 0])
+        self.adj[b].append([a, len(self.adj[a]) - 1, rcap, 0])
 
     def add_flow(self, vert, pos, aug):
-        nei, pair, cap, use = graph[vert][pos]
-        graph[vert][pos][3] += aug
-        graph[nei][pair][3] -= aug
+        nei, pair, cap, use = self.adj[vert][pos]
+        self.adj[vert][pos][3] += aug
+        self.adj[nei][pair][3] -= aug
 
-
-    def dfs(self, src, dest):
+    def dfs2(self, src, dest):
         aug, found = 0, False
 
         stack = [(src, INF)]
@@ -22,8 +24,8 @@ class Dinic:
             vert, limit = stack[-1]
 
             if found:
-                ptr[vert] -= 1
-                self.add_flow(vert, ptr[vert], aug)
+                self.ptr[vert] -= 1
+                self.add_flow(vert, self.ptr[vert], aug)
                 stack.pop()
                 continue
 
@@ -33,50 +35,55 @@ class Dinic:
                 stack.pop()
                 continue
 
-            if ptr[vert] < len(graph[vert]):
-                nei, pair, cap, use = graph[vert][ptr[vert]]
-                if lvl[nei] == lvl[vert] + 1 and cap > use:
+            if self.ptr[vert] < len(self.adj[vert]):
+                nei, pair, cap, use = self.adj[vert][self.ptr[vert]]
+                if self.lvl[nei] == self.lvl[vert] + 1 and cap > use:
                     stack.append((nei, min(limit, cap - use)))
-                ptr[vert] += 1
+                #print(f"finished vert {vert} at prt {self.ptr[vert]}")
+                self.ptr[vert] += 1
             else:
                 stack.pop()
         return aug
 
-    # Old version
-#        for pos in range(ptr[vert], len(graph[vert])):
-#            nei, pair, cap, use = graph[vert][pos]
-#            if lvl[nei] == lvl[vert] + 1:
-#                aug = self.dfs(nei, dest, min(limit, cap - use))
-#                if aug:
-#                    graph[vert][pos][3] += aug
-#                    graph[nei][pair][3] -= aug
-#                    return aug
-#            ptr[vert] += 1
-#
-#        return 0
+    def dfs(self, vert, dest, limit):
+        if vert == dest or not limit:
+            return limit
+
+        for pos in range(self.ptr[vert], len(self.adj[vert])):
+            nei, pair, cap, use = self.adj[vert][pos]
+            if self.lvl[nei] == self.lvl[vert] + 1:
+                aug = self.dfs(nei, dest, min(limit, cap - use))
+                if aug:
+                    self.adj[vert][pos][3] += aug
+                    self.adj[nei][pair][3] -= aug
+                    return aug
+            #print(f"finished vert {vert} at prt {self.ptr[vert]}")
+            self.ptr[vert] += 1
+
+        return 0
 
     def calc(self, src, dest):
-        cue = [0] * n
-        flow, cue[0] = 0, src
-        for l in range(31): # l = 30 maybe faster for random data
+        flow, self.cue[0] = 0, src
+        for l in range(31):  # l = 30 maybe faster for random data
             while True:
-                lvl, ptr = [0] * len(cue), [0] * len(cue)
-                qi, qe, lvl[src] = 0, 1, 1
-                while qi < qe and not lvl[dest]:
-                    vert = cue[qi]
+                #print("resetting ptr")
+                self.lvl, self.ptr = [0] * len(self.cue), [0] * len(self.cue)
+                qi, qe, self.lvl[src] = 0, 1, 1
+                while qi < qe and not self.lvl[dest]:
+                    vert = self.cue[qi]
                     qi += 1
-                    for nei, pair, cap, use in graph[vert]:
-                        if not lvl[nei] and (cap - use) >> (30 - l):
-                            cue[qe] = nei
+                    for nei, pair, cap, use in self.adj[vert]:
+                        if not self.lvl[nei] and (cap - use) >> (30 - l):
+                            self.cue[qe] = nei
                             qe += 1
-                            lvl[nei] = lvl[vert] + 1
+                            self.lvl[nei] = self.lvl[vert] + 1
 
-                aug = self.dfs(src, dest)
+                aug = self.dfs2(src, dest)
                 while aug:
                     flow += aug
-                    aug = self.dfs(src, dest)
+                    aug = self.dfs2(src, dest)
 
-                if not lvl[dest]:
+                if not self.lvl[dest]:
                     break
 
         return flow
